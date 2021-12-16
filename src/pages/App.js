4a670ad70854a2286db4067a49e738cb0885e939
ikolodiazhnyi <ikolodiazhnyi@gmail.com>
2021-12-16 10:53:51 +0200

    minor changes

--- src/pages/App.js
+++ src/pages/App.js
@@ -5,7 +5,7 @@ import SecondSection from "../components/SecondSection";
 import ThirdSection from "../components/ThirdSection";
 import FourthSection from "../components/FourthSection";
 import FifthSection from "../components/FifthSection";
-import SixthSection from "../components/SixthSection/SixthSection";
+import SixthSection from "../components/SixthSection";
 
 function App() {
   return (
