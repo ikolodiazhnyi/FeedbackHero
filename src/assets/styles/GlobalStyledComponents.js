54c47f95948b25bc1b79112a0cddd5438a84551f
ikolodiazhnyi <ikolodiazhnyi@gmail.com>
2021-12-20 10:19:43 +0200

    Move global styles to assets folder

---
+++ src/assets/styles/GlobalStyledComponents.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const AdvantageDescWrapper = styled.div`
+  text-align: center;
+  padding: 0 30px 0 30px;
+`;
+export const variables = {
+  breakpointPhoneSmall: "305px",
+  breakpointPhone: "767px",
+  breakpointTablet: "991px",
+  breakpointDesktop: "1109px",
+};
